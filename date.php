54ee39be60f979bae3b2a4eb2c8df538c134adb2
Sergey S <smirnof.sergey@gmail.com>
2020-08-13 15:18:41 +0300

    add function getWeekDay

--- date.php
+++ date.php
@@ -4,8 +4,9 @@
 class Date
 {
     private $date;
-    private $monthList=["01"=>"январь","02"=>"февраль","03"=>"март","04"=>"апрель","05"=>"май","06"=>"июнь","07"=>"июль","08"=>"август"
-    ,"09"=>"сентябрь","10"=>"октябрь","11"=>"ноябрь","12"=>"декабрь"];
+    private $monthList = ["01" => "январь", "02" => "февраль", "03" => "март", "04" => "апрель", "05" => "май", "06" => "июнь", "07" => "июль", "08" => "август"
+        , "09" => "сентябрь", "10" => "октябрь", "11" => "ноябрь", "12" => "декабрь"];
+    private $dayList=["0"=>"воскресенье","1"=>"понедельник","2"=>"вторник","3"=>"среда","4"=>"четверг","5"=>"пятница","6"=>"суббота"];
     public function __construct($date=null)
     {
         if ($date==null)
@@ -25,7 +26,7 @@ class Date
     public function getMonth($lang=null)
     {
         //по умолчанию возвращает номер месяца
-        //может принимать агрументы 'ru' и 'en' тогда возвращает название месяца словом на заданном языке
+        //может принимать агрументы 'ru' или 'en', в этом случае возвращает название месяца словом на заданном языке
         if ($lang==null)
         {
             return date ('m',$this->date);
@@ -54,7 +55,37 @@ class Date
         //возвращает год
         return date ('Y',$this->date);
     }
+    public function getWeekDay($lang=null)
+    {
+        //по умолчанию возвращает номер дня недели
+        //может принимать аргументы 'ru' или 'en', в этом случае возвращает день недели словом на заданном языке
+        if ($lang==null)
+        {
+            return date ('w',$this->date);
+        }
+        if ($lang=='ru')
+        {
+            foreach ($this->dayList as $key=>$value)
+            {
+                if ($key==date ('w',$this->date))
+                {
+                    return $value;
+                }
+            }
+        }
+        if ($lang=='en')
+        {
+            return date ('l',$this->date);
+        }
+        else
+        {
+            return 'incorrect argument $lang';
+        }
+    }
 
 }
-$obj=new Date('2045-01-30');
-echo $obj->getYear();
+$obj=new Date('2000-08-15');
+echo $obj->getDay().'<br>';
+echo $obj->getMonth('ru').'<br>';
+echo $obj->getYear().'<br>';
+echo $obj->getWeekDay('ru').'<br>';
